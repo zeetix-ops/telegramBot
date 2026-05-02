829fd3ec3e9f09626f542563e2c8136c196290ad
Maxim <greckimax54@gmail.com>
2026-05-02 13:38:03 +0300

    setupbot

---
+++ src/bot.ts
@@ -0,0 +1,19 @@
+import { Bot } from "grammy";
+const BOT_TOKEN:string = process.env.BOT_TOKEN as string
+// Create an instance of the `Bot` class and pass your bot token to it.
+const bot = new Bot(BOT_TOKEN); // <-- put your bot token between the ""
+
+// You can now register listeners on your bot object `bot`.
+// grammY will call the listeners when users send messages to your bot.
+
+// Handle the /start command.
+bot.command("start", (ctx) =>{ 
+    ctx.reply("Welcome! Up and running.")});
+// Handle other messages.
+bot.on("message", (ctx) => ctx.reply("Got another message!"));
+
+// Now that you specified how to handle messages, you can start your bot.
+// This will connect to the Telegram servers and wait for messages.
+
+// Start the bot.
+bot.start();
